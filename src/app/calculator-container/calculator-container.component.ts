01bd0b0c5c96e6aa1a12803a0d1d28d255afb0a2
Ilenia96 <ilenianese96@gmail.com>
2021-10-11 19:51:26 +0200

    fix code

--- src/app/calculator-container/calculator-container.component.ts
+++ src/app/calculator-container/calculator-container.component.ts
@@ -8,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorContainerComponent implements OnInit {
   DButtons: string[] = ['AC', 'CE'];
   buttons: string [] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '.', '0', '=', '+'];
+  result : string = '';
+  preValue : string = '';
+  curValue : string = '';
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addCalc(value : string) {
+if (this.result != '') {
+  this.preValue = this.curValue;
+  this.curValue = value;
+}
+
+if (value == 'AC') {
+  this.result = '';
+} else if (value == 'CE') {
+  this.result = this.preValue != '=' ? this.result.slice (0, -1) : this.result;
+} else if (value == '=') {
+  this.result = eval (this.result);
+} else {
+  this.result += value;
+}
   }
 
 }
